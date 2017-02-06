0f03bed8c44e9502ea1ca1e91f4a2e67921eef92
tianxq <tianxq@192.168.0.102>
2017-02-06 22:34:40 +0800

    reset flume file input stream

--- src/main/java/com/gsta/bigdata/etl/flume/ResettableFileInputStream.java
+++ src/main/java/com/gsta/bigdata/etl/flume/ResettableFileInputStream.java
@@ -286,9 +286,9 @@ public class ResettableFileInputStream extends ResettableInputStream
 
     CoderResult res = decoder.decode(buf, charBuf, isEndOfInput);
     if (res.isMalformed() || res.isUnmappable()) {
-      //res.throwException();
-      logger.error("file " + this.file.getName() + ",occur error:" + res.toString());
-      return -1;
+      res.throwException();
+      //logger.error("file " + this.file.getName() + ",occur error:" + res.toString());
+      //return -1;
     }
 
     int delta = buf.position() - start;
